7b717292bd04c1fb12d4b0cb6009a8c0d55e2038
nolorin <james.marlow.ma@gmail.com>
2021-09-07 16:23:12 -0400

    Testing

--- abstractions.php
+++ abstractions.php
@@ -2,14 +2,20 @@
 // Abstract data objects
 abstract class dataNode {
 	protected string $name;
-	protected string owner;
+	protected string $owner;
 	protected $source;
 	protected string $key;
 	public function __construct( string $name, string $owner = NULL, $source = NULL, string $key = NULL ) {
 		$this->name = $name;
-		$this->name = $owner;
-		$this->name = $source;
-		$this->name = $key;
+		if( !empty( $owner ) ) {
+			$this->owner = $owner; 
+		}
+		if( !empty( $source ) ) {
+			$this->source = $source;
+		}
+		if( !empty( $key ) ) {
+			$this->key = $key;
+		}
 	}
 	public function __get( string $name ) {
 		return $this->$name;
@@ -48,10 +54,18 @@ trait dataControl {
 			return parent::__get( $name );
 		}
 	}
+	public function set_cast( string $name, $example ) {
+		$this->casts[$name] = gettype( $example );
+		return TRUE;
+	}
+	public function unset_cast( string $name ) {
+		unset( $this->casts[$name] );
+		return TRUE;
+	}
 	public function set( string $name, $value, string $cast = NULL, $access = NULL ): bool {
 		if( !empty( $cast ) ) {
-			if( !is_callable( $this->casts[$cast] ) ) {
-				if( gettype( $value ) == gettype( $this->casts[$cast] ) ) {
+			if( !empty( $cast ) && !is_callable( $this->casts[$cast] ) ) {
+				if( gettype( $value ) == $this->casts[$cast] ) {
 					$this->vars[$name] = $value;
 					$output = TRUE;
 				} else {
@@ -69,7 +83,7 @@ trait dataControl {
 			}
 		} else {
 			$this->vars[$name] = $value;
-			$output = FALSE;
+			$output = TRUE;
 		}
 		if( $output && !empty( $access ) ) {
 			$this->access[$name] = $access;
@@ -78,7 +92,7 @@ trait dataControl {
 	}
 	public function clear( string $name, $access_code = NULL ): bool {
 		if( !empty( $this->access[$name] ) ) {
-			if( $this->access[$name]( $access_code ) ) {
+			if( $this->access[$name] == $access_code ) {
 				unset( $this->vars[$name] );
 				unset( $this->access[$name] );
 			} else {
@@ -93,21 +107,91 @@ trait dataControl {
 	}
 	public function get( string $name, $access_code = NULL ) {
 		if( !empty( $this->access[$name] ) ) {
-			if( $this->access[$name]( $access_code ) ) {
+			if( $this->access[$name] == $access_code ) {
 				return $this->vars[$name];
 			} else {
 				trigger_error( "Object property '$name' cannot be retrieved because access is denied", E_USER_NOTICE );
 				return NULL;
 			}
-		} else {
+		} else if( !empty( $this->vars[$name] ) ) {
 			return $this->vars[$name];
+		} else {
+			return NULL;
 		}
 	}
 }
-interface iterable {
+interface dataIterable {
 	public function add_item( $value, $key = NULL ): bool;
 	public function sub_item( $key ): bool;
 	public function filter( $value ): bool;
 	public function get_item( $key );
 	public function get_array(): array;
+}
+
+// Test 1
+class example extends dataNode {
+	public function test( $message ) {
+		echo "Test Message: " . $message . PHP_EOL;
+	}
+}
+
+// $wrong = new dataNode( 'object' ); // Returns error: PHP Fatal error:  Uncaught Error: Cannot instantiate abstract class dataNode
+
+$right = new example( 'object', 'tester' );
+var_dump( $right );
+$right->test( 'It works, damn it!' );
+
+echo PHP_EOL;
+
+// Test 2
+class datum extends dataNode {
+	use variables;
+}
+$test2 = new datum( 'object' );
+$test2->set( 'var1', 5 );
+$test2->set( 'var2', 10 );
+$test2->set( 'var3', 15 );
+$test2->clear( 'var2' );
+var_dump( $test2->get( 'var1') );
+var_dump( $test2->get( 'var2') );
+var_dump( $test2->get( 'var3') );
+var_dump( $test2 );
+
+echo PHP_EOL;
+
+// Test 3
+class uberDatum extends dataNode {
+	use dataControl;
+}
+$test3 = new uberDatum( 'object' );
+$test3->set_cast( 'number', 1234 );
+$test3->set( 'var1', 5, 'number' );
+$test3->set( 'var2', 10, NULL, 'UnAU21b' );
+$test3->set( 'var3', '15', 'number' );
+$test3->set( 'var4', 20 );
+$test3->clear( 'var2' );
+var_dump( $test3->get( 'var1') ); // Returns 5
+var_dump( $test3->get( 'var2') ); // Returns NULL, triggers Custom Error: PHP Notice:  Object property 'var2' cannot be retrieved because access is denied
+var_dump( $test3->get( 'var2', 'UnAU21b' ) ); // Returns 10
+var_dump( $test3->get( 'var3') ); // Returns NULL
+var_dump( $test3->get( 'var4') ); // Returns 20
+var_dump( $test3 );
+
+echo PHP_EOL;
+
+// Test 4
+class wrongImplement /*implements dataIterable*/ { // Returns error: PHP Fatal error:  Declaration of wrongImplement::sub_item($key) must be compatible with dataIterable::sub_item($key): bool
+	public function add_item( $value, $key = NULL ): bool {
+		return TRUE;
+	}
+	public function sub_item( $key ) { // Wrong return type
+		return NULL;
+	}
+	public function filter( $value, $cast = NULL ): bool { // Args don't match up
+		return FALSE;
+	}
+	// missing get_item method
+	public function get_array() { // Wrong return cast
+		return array();
+	} 
 }
--- css-gen.php
+++ css-gen.php
@@ -3,13 +3,13 @@
 class style {
 	protected array $attr = array();
 	protected string $type = '';
-	protected string $name;
+	public string $name;
 	public bool $verbose = FALSE;
-	public __construct( string $name, string $type = 'class' ) {
+	public function __construct( string $name, string $type = 'class' ) {
 		$this->name = $name;
 		$this->type( $type );
 	}
-	public __toString(): string {
+	public function __toString(): string {
 		$eol = $this->verbose ? PHP_EOL : '';
 		$ind = $this->verbose ? "\t" : '';
 		$spa = $this->verbose ? ' ' : '';
@@ -19,22 +19,6 @@ class style {
 		}
 		$output .= '}' . $eol;
 		return $output;
-	}
-	public function __set( string $prop, $value ) {
-		if( in_array( $value, [ 'name', 'type' ] ) ) {
-			$this->$prop = $value;
-			return TRUE;
-		} else {
-			return parent::__set( $prop, $value );
-		}
-	}
-	public function unset( string $attr ) {
-		if( in_array( $attr, [ 'name', 'type' ] ) ) {
-			$this->$attr = '';
-			return TRUE;
-		} else {
-			return parent::__unset( $attr );
-		}
 	}
 	public function set( string $attr, $value ) {
 		if( is_string( $value ) || is_numeric( $value ) ) {
@@ -51,7 +35,7 @@ class style {
 		unset( $this->attr[$attr] );
 		return TRUE;
 	}
-	pubilic function type( string $type ) {
+	public function type( string $type ) {
 		switch( $type ) {
 			case 'class':
 				$this->type = '.'; break;
@@ -63,3 +47,18 @@ class style {
 		return TRUE;
 	}
 }
+
+// Test
+$style = new style( 'block', 'class' );
+$style->set( 'color', 'red' );
+$style->set( 'padding', '2px 2.5px' );
+echo $style . PHP_EOL . PHP_EOL;
+$style->clear( 'padding' );
+$style->set( 'margin', '2px' );
+$style->verbose = TRUE;
+echo $style . PHP_EOL;
+$style->set( 'border-radius', '10px' );
+$style->type( 'id' );
+$style->verbose = FALSE;
+echo $style . PHP_EOL;
+
